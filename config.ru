7d2c3560798a019c4625ef22b4bf5f080bba864e
Alisa Truzhenko <truealisa@gmail.com>
2018-07-13 23:12:53 +0300

    Add Rack::Sessions::Cookie

--- config.ru
+++ config.ru
@@ -1,6 +1,8 @@
+require 'securerandom'
 require 'codebreaker_app'
 
 use Rack::Reloader, 0
+use Rack::Session::Cookie, path: '/', secret: SecureRandom.base64(10)
 use Rack::Static, :urls => ["/stylesheets"], :root => "public"
 
-run CodebreakerApp.new
+run CodebreakerApp
--- lib/codebreaker_app.rb
+++ lib/codebreaker_app.rb
@@ -3,25 +3,30 @@ require 'bundler/setup'
 require 'rg_hw_codebreaker'
 
 class CodebreakerApp
-  def initialize
-    @flash = { error: nil, notice: nil }
+  def self.call(env)
+    new(env).response.finish
   end
 
-  def call(env)
-    request = Rack::Request.new(env)
-    case request.path
-    when '/' then Rack::Response.new(render('index.html.erb'))
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
+
+  def response
+    case @request.path
+    when '/'
+      @request.session.clear
+      Rack::Response.new(render('index.html.erb'))
     when '/help' then Rack::Response.new(render('help.html.erb'))
     when '/best_results' then Rack::Response.new(render('best_results.html.erb'))
     when '/game'
-      start_game unless @game
+      start_game unless @request.session[:game]
       Rack::Response.new(render('game.html.erb'))
     when '/submit_guess'
-      submit_guess(request.params['guess'])
+      submit_guess(@request.params['guess'])
       Rack::Response.new { |response| response.redirect('/game') }
     when '/enter_name'
-      enter_name(request.params['name'])
-      Rack::Response.new { |response| response.redirect('/game') }
+      enter_name(@request.params['name'])
+      Rack::Response.new { |response| response.redirect('/best_results') }
     when '/show_hint'
       show_hint
       Rack::Response.new { |response| response.redirect('/game') }
@@ -34,35 +39,34 @@ class CodebreakerApp
     ERB.new(File.read(path)).result(binding)
   end
 
-  def break_the_lines(text)
-    text.to_s.gsub(/\n/, '<br/>')
-  end
-
   def start_game
-    @game = nil if @game
-    @game = RgHwCodebreaker::Game.new
-    @game.start
-    @attempts = []
-    @show_hint = false
+    @request.session[:game] = RgHwCodebreaker::Game.new
+    @request.session[:game].start
+    @request.session[:attempts] = []
+    @request.session[:show_hint] = false
   end
 
   def submit_guess(guess)
-    if @game.valid_guess?(guess)
-      guess_result = @game.check_guess(guess)
-      @attempts << { guess: guess, guess_result: guess_result }
+    if @request.session[:game].valid_guess?(guess)
+      guess_result = @request.session[:game].check_guess(guess)
+      @request.session[:attempts] << { guess: guess, guess_result: guess_result }
     else
-      @flash[:error] = 'Your input is invalid'
+      @request.session[:error_msg] = 'Your input is invalid'
     end
   end
 
   def enter_name(player_name)
-    current_result = [player_name, Date.today, 10 - @game.turns]
+    current_result = [player_name, Date.today, 10 - @request.session[:game].turns]
     RgHwCodebreaker::Cli.new.write_result_to_file(current_result)
-    @flash[:notice] = 'Your result saved'
+    @request.session[:notice_msg] = 'Your result saved'
   end
 
   def show_hint
-    @show_hint = true
-    @hint = @game.any_hints_left? ? "Hint: #{@game.give_a_hint}***" : 'No hints left :('
+    @request.session[:show_hint] = true
+    @request.session[:hint] = if @request.session[:game].any_hints_left?
+                                "Hint: #{@request.session[:game].give_a_hint}***"
+                              else
+                                'No hints left :('
+                              end
   end
 end
